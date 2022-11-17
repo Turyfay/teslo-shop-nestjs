a3a918ba497d88285edc0253ea6e26ea7aa8af19
David Enamorado <david8enaceituno@gmail.com>
2022-11-17 16:38:27 -0600

    add: entidad sin relacion

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,28 @@
+import { Column } from 'typeorm/decorator/columns/Column';
+import { PrimaryGeneratedColumn } from 'typeorm/decorator/columns/PrimaryGeneratedColumn';
+import { Entity } from 'typeorm/decorator/entity/Entity';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column('text', { unique: true })
+  title: string;
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+  @Column('text', { unique: true })
+  slug: string;
+  @Column('int', { default: 0 })
+  stock: number;
+  @Column('text', { array: true })
+  sizes: string[];
+  @Column('text')
+  gender: string;
+}
